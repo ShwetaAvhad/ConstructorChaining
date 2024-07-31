4326fa8804ba6b25bc9aa7d7416d22afbeb7845b
ShwetaAvhad <124449012+ShwetaAvhad@users.noreply.github.com>
2024-07-31 22:33:50 +0530

    Add files via upload

---
+++ EnumDemo/EnumDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDemo
+{
+    enum WeekDays
+    {
+        Monday=1,
+        Tuesday=2,
+        Wednesday=3,
+        Thursday=4,
+        Friday=5,
+        Saturday=6,
+        Sunday=7
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //WeekDays weekdays=WeekDays.Friday;
+            //Console.WriteLine((int)weekdays);
+            foreach (var days in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(days +" ==> "+(int)days);
+            }
+
+        }
+    }
+}
