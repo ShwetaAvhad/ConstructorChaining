d54a6d47612fe49e0366445e21950ebd94f4a986
ShwetaAvhad <124449012+ShwetaAvhad@users.noreply.github.com>
2024-07-31 22:31:44 +0530

    Add files via upload

---
+++ BMICalculator/BMICalculator/Person.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{
+    public class Person
+    {
+        public int PersonId { get; set; }
+        public string PersonName { get; set; }
+        public int PersonAge { get; set; }
+        public float PersonHeight { get; set; }
+        public float PersonWeight { get; set; }
+        public double PersonBMI;
+        public const float MIN_HEIGHT = 5;
+        public const float MIN_WEIGHT = 50;
+        public Person(int personId, string personName, int personAge)
+        {
+            PersonId = personId;
+            PersonName = personName;
+            PersonAge = personAge;
+            if (PersonHeight < MIN_HEIGHT)
+            {
+                PersonHeight = MIN_HEIGHT;
+            }
+            if (PersonWeight < MIN_WEIGHT)
+            {
+                PersonWeight = MIN_WEIGHT;
+            }
+        }
+        public Person(int personId, string personName, int personAge, float personHeight, float personWeight) 
+                    : this(personId, personName, personAge)
+        {
+            PersonHeight = personHeight;
+            PersonWeight = personWeight;
+        }
+        public double CalculateBMIScore(float personHeight, float personWeight)
+        {
+            double heightInMeters = personHeight * 0.3048;
+            double bmiScore = personWeight / (heightInMeters * heightInMeters);
+            PersonBMI = bmiScore;
+            return PersonBMI;
+        }
+        public string BodyTypeBasedOnBMI(double bmiscore)
+        {
+            string result = "";
+            if (bmiscore > 17 && bmiscore< 18.4)
+            {
+                result="UnderWeight";
+            }
+            else if (bmiscore > 18.5 && bmiscore < 24.9)
+            {
+                result="Normal";
+            }
+            else if (bmiscore > 25 && bmiscore < 29.9)
+            {
+                result="OverWeight";
+            }
+            else
+            {
+                result="Obese";
+            }
+            return result;
+        }
+        public override string ToString()
+        {
+            return $"Person Id : {PersonId}\n" +
+                   $"Person Name : {PersonName}\n" +
+                   $"Person Age : {PersonAge}\n" +
+                   $"Person Height : {PersonHeight}\n" +
+                   $"Person Weight : {PersonWeight}";                   
+        }
+    }
+    }
---
+++ BMICalculator/BMICalculator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator
+{    
+    public class Program
+    {
+        public static void PrintDetails(Person person)
+        {
+            Console.WriteLine(person);
+            double bmiScore1 = person.CalculateBMIScore(person.PersonHeight, person.PersonWeight);
+            Console.WriteLine("BMI Score : "+bmiScore1);
+            string bodyType = person.BodyTypeBasedOnBMI(bmiScore1);
+            Console.WriteLine("Body Type : "+bodyType);
+        }
+        static void Main(string[] args)
+        {
+            Person[] person1 = { new Person(1, "Ram", 18),
+                                 new Person(2, "Shyam", 20,8,30),
+                                 new Person(3,"Sachin",22,4,40),
+                                 new Person(4,"Akshay",17,9,45)};
+
+            foreach(Person person2 in person1)
+            {
+                PrintDetails(person2);
+                Console.WriteLine();
+            }
+
+            double maximumBMI = 0;
+            Person person3 = null;
+            foreach(Person person in person1)
+            {
+                if (person.PersonBMI>maximumBMI)
+                {
+                    maximumBMI = person.PersonBMI;
+                    person3= person;
+                }
+            }           
+            Console.WriteLine("Person Having Maximum BMI : ");
+            PrintDetails(person3);
+            Console.WriteLine();         
+
+        }
+    }
+}
+
