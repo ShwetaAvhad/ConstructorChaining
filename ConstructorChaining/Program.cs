278cbb69ed219820d74cb2ab05356c3b6046ebab
Shweta.Avhad <Shweta.Avhad@DELL1540>
2024-07-31 22:29:39 +0530

    Add project files.

---
+++ ConstructorChaining/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    internal class Program
+    {
+        public static void PrintDetails(Student student)
+        {
+            Console.WriteLine(student.Print());
+        }
+        static void Main(string[] args)
+        {            
+            Student student1 = new Student(1,"Ram","Computer");
+            Student student2 = new Student(2,"Shyam","Mechanical",16,4);
+            Student student3 = new Student(3,"Akshay","IT",6,8);
+
+            PrintDetails(student1);
+            PrintDetails(student2);
+            PrintDetails(student3);
+
+        }
+    }
+}
---
+++ ConstructorChaining/Student.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorChaining
+{
+    internal class Student
+    {
+        public int studentId;
+        public string studentName;
+        public string studentBranch;
+        public int studentAge;        
+        public float studentHeight;
+        public float studentWeight;
+        public const int MIN_AGE = 10;
+        public const int MIN_HEIGHT = 5;
+                
+        public Student(int studentId, string studentName, string studentBranch)
+        {
+            this.studentId = studentId;
+            this.studentName = studentName;
+            this.studentBranch = studentBranch;  
+            if(studentAge<MIN_AGE)
+            {
+                studentAge = MIN_AGE;
+            }
+            if (studentHeight < MIN_HEIGHT)
+            {
+                studentHeight= MIN_HEIGHT;
+            }
+        }
+        public Student(int studentId, string studentName, string studentBranch, int studentAge, float studentHeight) : this(studentId, studentName, studentBranch)
+        {
+            if (studentAge < MIN_AGE)
+                studentAge = MIN_AGE;
+            else
+                this.studentAge = studentAge;
+
+            if(studentHeight < MIN_HEIGHT)
+                studentHeight = MIN_HEIGHT;
+            else
+                this.studentHeight = studentHeight;
+        }
+        public string Print()
+        {
+            return $"Student ID : {studentId}\n" +
+                   $"Student Name : {studentName}\n" +
+                   $"Student Branch : {studentBranch}\n" +
+                   $"Student Age : {studentAge}\n" +
+                   $"Student Height : {studentHeight}\n";                   
+        }
+    }
+}
